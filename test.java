944d1be8aa64466888540d81a9eee90d0a4b9981
ikbalisk <61731203+ikbalisk@users.noreply.github.com>
2020-03-11 19:40:15 +0800

    Add files via upload

---
+++ test.java
@@ -0,0 +1,8 @@
+public class test {
+	public static void main(String[] args){
+		System.out.print("selamat belajar\n");
+		System.out.print("Belajar bahasa pemrograman java\n");
+		System.out.print("Selesai belajar pemrograman java");
+		
+}
+}
